f2a3da5b1ebea86628161c4753a5db4015c50562
dzaky1201 <dzakyhdr@gmail.com>
2022-02-10 07:50:27 +0700

    starter

--- app/src/main/java/com/example/optionmenuinactionbar/MainActivity.kt
+++ app/src/main/java/com/example/optionmenuinactionbar/MainActivity.kt
@@ -14,27 +14,13 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        val inflater = menuInflater
-        inflater.inflate(R.menu.option_menu, menu)
+
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
-            R.id.menu1 -> {
-                Toast.makeText(this, "Tombol Share Di Klik", Toast.LENGTH_SHORT).show()
-                true
-            }
-            R.id.menu2 -> {
-                Toast.makeText(this, "Tombol Menu 2 di klik", Toast.LENGTH_SHORT).show()
-                true
-            }
-            R.id.menu3 -> {
-                val i = Intent(this, SecondActivity::class.java)
-                startActivity(i)
-                true
-            }
-            else -> true
-        }
+
+        return true
+
     }
 }
